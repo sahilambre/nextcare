fdcc41f6569e9a7f839490b5920f5c93fd2a114c
Sahil Mukesh Ambre <59484827+sahilambre@users.noreply.github.com>
2024-09-30 18:29:38 -0400

    :art: create homepage #1

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -5,7 +5,12 @@ import { z } from "zod";
 import { Form } from "@/components/ui/form";
 import CustomFormField from "../CustomFormField";
 import { Button } from "../ui/button";
-
+import SubmitButton from "../SubmitButton";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+import { create } from "domain";
+import { Router } from "next/router";
+import { useRouter } from "next/navigation";
 export enum FormFieldType {
   INPUT = "input",
   TEXTAREA = "textarea",
@@ -16,24 +21,37 @@ export enum FormFieldType {
   SKELETON = "skeleton",
 }
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
-
 const PatientForm = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+  async function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+    try {
+      //   const userData = {
+      //     name,
+      //     email,
+      //     phone,
+      //   };
+      //   const user = await createUser(userData);
+      //   if (user) router.push(`/patients/${user.$id}/register`);
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
     <Form {...form}>
@@ -70,7 +88,7 @@ const PatientForm = () => {
           iconSrc="/assets/icons/phone.svg"
           iconAlt="user"
         />
-        <Button type="submit">Submit</Button>
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
   );
